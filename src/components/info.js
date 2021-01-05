3bcbbc136befe5f38b39a6e4ef9f9efbfb0085b1
Farhan Roy <farhanroy120@gmail.com>
2021-01-05 20:52:55 +0700

    Update info.js

--- src/components/info.js
+++ src/components/info.js
@@ -14,7 +14,7 @@ export default function info() {
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
         <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Roy.
+        Farhan Roy.
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
